aeda8d6035c9ce7f3ab49af17d0e956c32f520ba
andrros3030 <66036268+andrros3030@users.noreply.github.com>
2023-01-14 23:59:58 +0300

    comments for get_plot

--- src/drawer.py
+++ src/drawer.py
@@ -2,9 +2,10 @@ import io
 import matplotlib.pyplot as plt
 
 
+# функция которая возвращает изображение, которое потом можно отправить сообщением
 def get_plot(x, y):
-    my_stringIObytes = io.BytesIO()
-    plt.plot(x, y)
-    plt.savefig(my_stringIObytes, format='jpg')
-    my_stringIObytes.seek(0)
-    return my_stringIObytes
+    my_stringIObytes = io.BytesIO()  # объект, который хранит данные изображения
+    plt.plot(x, y)  # строим по x'ам и y'кам график
+    plt.savefig(my_stringIObytes, format='jpg')  # сохраняем изображение графика в my_stringIObytes в формате jpg
+    my_stringIObytes.seek(0)  # не знаю че такое, наверное что-то важное
+    return my_stringIObytes  # возвращаем объект хранящий изображение
