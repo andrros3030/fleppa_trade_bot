869fa271c424d8f55ed405867d13e5b8982315b4
andrros3030 <66036268+andrros3030@users.noreply.github.com>
2023-01-15 14:10:23 +0300

    logger test + more channels

--- src/logger.py
+++ src/logger.py
@@ -1,50 +1,58 @@
-def error_color():
-    return '\033[95m'
-
-
-def verbose_color():
-    return '\033[96m'
+def yellow_color():
+    return '\033[93m'
 
 
 def pink_color():
+    return '\033[95m'
+
+
+def light_blue_color():
+    return '\033[96m'
+
+
+def red_color():
     return '\033[91m'
 
 
-def green_color():
+def dark_green_color():
     return '\033[92m'
 
 
-def blue_color():
+def dark_blue_color():
     return '\033[94m'
 
 
 class Logger:
     log_levels: dict = {
         0: True,  # This is "error" logging level
-        1: False  # This is "verbose" logging level
+        1: False,  # This is "warning" logging level
+        2: False,  # This is "info" logging level
+        3: False  # This is "verbose" logging level
     }
     log_level_color: dict = {
-        0: error_color(),
-        1: verbose_color(),
+        0: red_color(),
+        1: pink_color(),
+        2: light_blue_color(),
+        3: dark_blue_color(),
         # HEADER = '\033[95m'
-        # OKBLUE = '\033[94m'
         # OKCYAN = '\033[96m'
-        # OKGREEN = '\033[92m'
-        # WARNING = '\033[93m'
-        # FAIL = '\033[91m'
-        # ENDC = '\033[0m'
         # BOLD = '\033[1m'
         # UNDERLINE = '\033[4m'
     }
     log_levels_names: dict = {
-        0: 'LOGGER/ERROR: ',
-        1: 'LOGGER/VERBOSE: '
+        0: 'LOGGER/ERROR:   ',
+        1: 'LOGGER/WARNING: ',
+        2: 'LOGGER/INFO:    ',
+        3: 'LOGGER/VERBOSE: ',
     }
 
-    def __init__(self, is_poduction: bool):
+    def __init__(self, is_poduction: bool, base_func=print):
         self.is_production = is_poduction
+        self.base_func = base_func
         if not is_poduction:
             self.log_levels[1] = True
+            self.log_levels[2] = True
+            self.log_levels[3] = True
         self.v(
             f"Logger set up complete, ready to log, prod: {is_poduction}"
         )
@@ -53,10 +61,16 @@ class Logger:
         real_color = override_color if override_color is not None else self.log_level_color[level]
         end_char = '\r' if self.is_production else '\n'
         if self.log_levels[level]:
-            print(real_color + self.log_levels_names[level] + str(message) + '\033[0m', end=end_char)
+            return self.base_func(real_color + self.log_levels_names[level] + str(message) + '\033[0m', end=end_char)
 
     def v(self, message: str, override_color: str = None):
-        self.__log(message, level=1, override_color=override_color)
+        return self.__log(message, level=3, override_color=override_color)
+
+    def i(self, message: str, override_color: str = None):
+        return self.__log(message, level=2, override_color=override_color)
+
+    def w(self, message: str, override_color: str = None):
+        return self.__log(message, level=1, override_color=override_color)
 
     def e(self, message: str, override_color: str = None):
-        self.__log(message, level=0,  override_color=override_color)
+        return self.__log(message, level=0,  override_color=override_color)
