5ee8452f3a6d9abdafabf5feded7528a9f34a276
vlabusa <vlabusa@gmail.com>
2023-01-18 23:25:03 +0300

    Update homiak_deploma.py

--- src/homiak_deploma.py
+++ src/homiak_deploma.py
@@ -9,8 +9,8 @@ aboba = name + xcv + surname
 url = "https://storage.yandexcloud.net/telegram-trade-bot/homyak_diploma.jpg"
 image = Image.open(urlopen(url))
 
-font = ImageFont.truetype("arial.ttf", 45)
+font = ImageFont.truetype("arial.ttf", 75)
 drawer = ImageDraw.Draw(image)
-drawer.text((600, 1100), aboba, font=font, fill='black')
+drawer.text((500, 1070), aboba, font=font, fill='black')
 
 image.show()
