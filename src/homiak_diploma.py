ae2e444b61ba7e6bc02ea015121feebf92944e72
andrros3030 <66036268+andrros3030@users.noreply.github.com>
2023-01-21 00:51:36 +0300

    urlib to requests migration

--- src/homiak_diploma.py
+++ src/homiak_diploma.py
@@ -1,11 +1,12 @@
 from PIL import Image, ImageDraw, ImageFont
-from urllib.request import urlopen
-import io
+import requests
+from io import BytesIO
 
 
 def diploma(name):
     url = "https://storage.yandexcloud.net/telegram-trade-bot/homyak_diploma.jpg"
-    image = Image.open(urlopen(url))
+    url_image = requests.get(url)
+    image = Image.open(BytesIO(url_image.content))
 
     font = ImageFont.truetype("arial.ttf", 75)
     drawer = ImageDraw.Draw(image)
@@ -13,7 +14,7 @@ def diploma(name):
     drawer.text(((1600 - txtwidth) / 2, 1070), name, fill="black", font=font)
 
     # функция которая возвращает изображение, которое потом можно отправить сообщением
-    my_stringIObytes = io.BytesIO()  # объект, который хранит данные изображения
+    my_stringIObytes = BytesIO()  # объект, который хранит данные изображения
     image.save(my_stringIObytes, format='jpeg')  # сохраняем изображение графика в my_stringIObytes в формате jpg
     my_stringIObytes.seek(0)  # не знаю че такое, наверное что-то важное
     return my_stringIObytes  # возвращаем объект хранящий изображение
