87a943ba1b2ae2495ed9b3948bbcc171668f56b2
vlabusa <vlabusa@gmail.com>
2023-01-26 01:57:27 +0300

    Координаты печати

--- src/common_modules/homiak_diploma.py
+++ src/common_modules/homiak_diploma.py
@@ -10,6 +10,11 @@ from os import path
 def diploma(name, totem):
     font_path = path.dirname(__file__) + '/../arial.ttf'
     url = "https://storage.yandexcloud.net/telegram-trade-bot/homyak_diploma.jpg"
+
+    url1 = "https://storage.yandexcloud.net/telegram-trade-bot/fleppa_trademark.png"
+    url1_image = requests.get(url1)
+    image1 = Image.open(BytesIO(url1_image.content))
+
     try:
         url_image = requests.get(url)
         image = Image.open(BytesIO(url_image.content))
@@ -26,6 +31,8 @@ def diploma(name, totem):
 
     txtwidth = drawer.textsize(totem, font=font)[0]
     drawer.text(((1600 - txtwidth) / 2, 1560), totem, fill="black", font=font)
+
+    image.paste(image1, (397, 1584), image1.convert('RGBA'))
 
     # функция которая возвращает изображение, которое потом можно отправить сообщением
     my_stringIObytes = BytesIO()  # объект, который хранит данные изображения
