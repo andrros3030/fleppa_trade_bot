5ff48e735c1541da41697783cd4a477e0461c612
savageego <121581597+savageego@users.noreply.github.com>
2023-01-27 17:27:17 +0300

    Update request_currency.py

--- src/request_currency.py
+++ src/request_currency.py
@@ -3,25 +3,33 @@ import requests
 
 
 def currency_info(currency_ticker):
-    today = datetime.now()
-    last_trade_day = today - timedelta(days=1)
 
     result = {}
     for currency in currency_ticker:
-        response_currency = requests.get(
-            f'https://iss.moex.com/iss/engines/currency/markets/index/securities/{currency}'
-            f'FIX/trades.json?reversed=1&limit=1')
-        currency_data_today = response_currency.json()['trades']['data'][0][-1]
+        url_today = f'https://iss.moex.com/iss/engines/currency/markets/index/securities/{currency}' \
+                    f'FIX/trades.json?reversed=1&limit=1'
+        try:
+            response_currency = requests.get(url_today)
+            currency_data_today = response_currency.json()['trades']['data'][0][-1]
+            today = response_currency.json()['trades']['data'][0][-4]
+
+        except Exception:
+            raise Exception(f"Can't get data with this URL: {url_today}")
 
         currency_value = []
+        last_trade_day = datetime.strptime(today, '%Y-%m-%d') - timedelta(days=1)
+        url_last_trade_date = f"https://iss.moex.com/iss/history/engines/currency/markets" \
+                              f"/index/securities.json?date={last_trade_day}"
         while len(currency_value) == 0:
-            response_currency = requests.get(
-                f"https://iss.moex.com/iss/history/engines/currency/markets/index/securities.json?date="
-                f"{last_trade_day.strftime('%Y-%m-%d')}")
-            currency_columns = response_currency.json()['history']['columns']
-            currency_value = response_currency.json()['history']['data']
+            try:
+                response_currency = requests.get(url_last_trade_date)
+                currency_columns = response_currency.json()['history']['columns']
+                currency_value = response_currency.json()['history']['data']
+            except Exception:
+                raise Exception(f"Can't get data with this URL: {url_last_trade_date}")
 
-            last_trade_day -= timedelta(days=1)
+            if len(currency_value) == 0:
+                last_trade_day -= timedelta(days=1)
 
         for i in currency_value:
             if f'{currency}FIX' in i:
@@ -36,9 +44,8 @@ def currency_info(currency_ticker):
         else:
             currency_content = f'{currency}: {currency_data_today} ({currency_change} % ⚪)'
 
-        result.update({f"{currency.upper()}": {'value': currency_data_today, 'change': f'{currency_change}',
-                                               'full_info': f'{currency_content}'}})
-
-        last_trade_day = today - timedelta(days=1)
+        result.update({f'{currency.upper()}': {'value': currency_data_today, 'change': f'{currency_change}',
+                                               'full_info': f'{currency_content}'}, 'trade_day': f'{today}',
+                       'trade_date_before': f'{last_trade_day.strftime("%m-%d-%Y")}'})
 
     return result
