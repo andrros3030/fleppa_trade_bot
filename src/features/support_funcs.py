00da900b3d0f6be69b904933975652bb49372f1a
Emil Mukhamedov <emilmukh-olimp@yandex.ru>
2023-01-31 17:46:02 +0300

    add stats_command

--- src/features/support_funcs.py
+++ src/features/support_funcs.py
@@ -50,3 +50,36 @@ def make_request(cc: CallContext):
     _, method, resource_link = map(str, cc.splitted_message)
     res = requests.request(method, resource_link)
     cc.bot.send_message(cc.chat_id, str(res.status_code) + ': ' + str(res.headers) + '\n' + str(res.reason))
+
+
+def stats(cc: CallContext):
+    users_query = 'select count(*) as total, ' \
+                  'sum(case when current_date - cast(ts_reg as date) <= 1 then 1 else 0 end) as Daily, ' \
+                  'sum(case when current_date - cast(ts_reg as date) <= 7 then 1 else 0 end) as Weekly, ' \
+                  'sum(case when current_date - cast(ts_reg as date) <= 30 then 1 else 0 end) as Monthly ' \
+                  'from t_users;'
+    messages_query = 'select count(*) as total, ' \
+                     'sum(case when current_date - cast(ts_saved as date) <= 1 then 1 else 0 end) as Daily, ' \
+                     'sum(case when current_date - cast(ts_saved as date) <= 7 then 1 else 0 end) as Weekly, ' \
+                     'sum(case when current_date - cast(ts_saved as date) <= 30 then 1 else 0 end) as Monthly ' \
+                     'from t_messages;'
+    fb_query = 'select count(*) as total, ' \
+               'sum(case when current_date - cast(ts_requested as date) <= 1 then 1 else 0 end) as Daily, ' \
+               'sum(case when current_date - cast(ts_requested as date) <= 7 then 1 else 0 end) as Weekly, ' \
+               'sum(case when current_date - cast(ts_requested as date) <= 30 then 1 else 0 end) as Monthly ' \
+               'from t_feedback;'
+    users_query = cc.database.unsafe_exec(users_query)
+    messages_query = cc.database.unsafe_exec(messages_query)
+    fb_query = cc.database.unsafe_exec(fb_query)
+    return cc.bot.send_message(cc.chat_id, f'Пользователей: {users_query[0][0]} '
+                                           f'(+{users_query[0][1]} за день/ '
+                                           f'+{users_query[0][2]} за неделю/'
+                                           f'+{users_query[0][3]} за месяц).\n'
+                                           f'Сообщений: {messages_query[0][0]} '
+                                           f'(+{messages_query[0][1]} за день/ '
+                                           f'+{messages_query[0][2]} за неделю/ '
+                                           f'+{messages_query[0][3]} за месяц).\n'
+                                           f'Фидбэка: {fb_query[0][0]} '
+                                           f'(+{fb_query[0][1]} за день/ '
+                                           f'+{fb_query[0][2]} за неделю/ '
+                                           f'+{fb_query[0][3]} за месяц).')
